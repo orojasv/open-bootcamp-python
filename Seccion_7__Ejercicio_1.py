9e3d1242a8cb39e013155d2c8c99eb9bce83625e
Oscar Rojas <rojasunalcol@gmail.com>
2022-04-29 05:30:28 -0500

    Open BootCamp - Python - Seccion 7 - Ejercicio 1

---
+++ Seccion_7__Ejercicio_1.py
@@ -0,0 +1,9 @@
+import operaciones as o
+
+a = 10
+b = 2
+
+print("Suma:", o.sumar(a, b))
+print("Resta:", o.restar(a, b))
+print("Multiplicación:", o.multiplicar(a, b))
+print("División:", o.dividir(a, b))
